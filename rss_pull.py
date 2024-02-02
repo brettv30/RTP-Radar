d63a1f2cdd83094828e5d362c8dc8a1e3fd7bdad
brettv30 <brettvogelsang30@gmail.com>
2024-02-01 20:52:40 -0500

    Added function to convert published dates to usable format
    - clean_published_dates parses the published date and turns it into a useful datetime variable
    - build_dataframe is a function that builds a pandas dataframe based on the lists that are passed to it

--- rss_pull.py
+++ rss_pull.py
@@ -2,33 +2,39 @@ import feedparser
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from datetime import datetime
 
 feed_list = [
-    # "http://www.wral.com/news/rss/142/",
+    "http://www.wral.com/news/rss/142/",
     # "https://www.durhamnc.gov/RSSFeed.aspx?ModID=76&CID=All-0",
     # "https://abc11.com/feed/",
-    "https://reddit.com/r/raleigh/new/.rss?sort=new",
+    # "https://www.dailytarheel.com/plugin/feeds/tag/pageOne"
+    # "https://reddit.com/r/raleigh/new/.rss?sort=new",
     # "https://reddit.com/r/chapelhill/new/.rss?sort=new",
     # "https://reddit.com/r/bullcity/new/.rss?sort=new",
-    # "https://www.townofchapelhill.org/Home/Components/RssFeeds/RssFeed/View?ctID=5&cateIDs=6",
-    # "https://www.townofchapelhill.org/Home/Components/RssFeeds/RssFeed/View?ctID=5&deptIDs=15",
-    # "https://www.townofchapelhill.org/Home/Components/RssFeeds/RssFeed/View?ctID=6&deptIDs=10"
-    # "https://www.townofchapelhill.org/Home/Components/RssFeeds/RssFeed/View?ctID=5&cateIDs=1%2c2%2c6%2c8%2c9%2c12%2c14%2c15%2c18%2c71%2c72%2c73%2c74%2c86%2c105%2c108%2c117%2c120%2c124%2c193%2c199%2c204%2c261%2c274%2c277%2c286%2c289%2c290%2c291%2c292%2c293%2c294%2c296%2c299%2c301",
 ]
 
-feed_all = []
+all_urls = []
 authors = []
 published = []
-title = []
+titles = []
 article_content = []
 
 for source in feed_list:
     feed = feedparser.parse(source)
     published.extend(f"{item.published}" for item in feed.entries)
-    authors.extend(f"{item.author}" for item in feed.entries)
-    feed_all.extend(f"{item.link}" for item in feed.entries)
 
-for url in feed_all:
+    try:
+        authors.extend(f"{item.author}" for item in feed.entries)
+    except AttributeError:
+        authors.extend("Unknown")
+
+    all_urls.extend(f"{item.link}" for item in feed.entries)
+
+# count = 0
+for url in all_urls:
+    # if count == 3:
+    #     break
     page = requests.get(url).text
     soup = BeautifulSoup(page, "html.parser")
     description = soup.find_all("h1")
@@ -42,20 +48,39 @@ for url in feed_all:
     header = "".join(f"{content.get_text()}" for content in description)
 
     if "reddit" in url:
-        title.append(f"{header}")
+        titles.append(f"{header}")
     else:
-        title.append(f"{soup.title.get_text()}")
+        titles.append(f"{soup.title.get_text()}")
 
-df_dict = {
-    "published": published,
-    "authors": authors,
-    "feed_all": feed_all,
-    "title": title,
-    "article_content": article_content,
-}
+#  count += 1
 
-df = pd.DataFrame(
-    df_dict, columns=["published", "authors", "feed_all", "title", "article_content"]
-)
 
-print(df.head())
+def build_dataframe(published, authors, all_urls, title, article_content):
+    df_dict = {
+        "published": published,
+        "authors": authors,
+        "urls": all_urls,
+        "title": title,
+        "article_content": article_content,
+    }
+
+    return pd.DataFrame(
+        df_dict, columns=["published", "authors", "urls", "title", "article_content"]
+    )
+
+
+def clean_published_dates(date_list):
+    # Define the date format
+    date_format = "%a, %d %b %Y %H:%M:%S %z"
+
+    # Parse each string into a datetime object
+    parsed_dates = [
+        datetime.strptime(date_string, date_format) for date_string in date_list
+    ]
+
+    # Convert datetime objects to strings in the desired format
+    return [date.strftime("%Y-%m-%d %H:%M:%S") for date in parsed_dates]
+
+
+print(authors)
+print(titles)
