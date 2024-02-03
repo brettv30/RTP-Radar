d2cb5d717d6db59c4c94a00c9e9f09c200de2299
brettv30 <brettvogelsang30@gmail.com>
2024-02-03 18:18:50 -0500

    Overhauled rss_pull_upgraded to follow better OOP principles
    - Create the URLParser class which is used to parse out headers and content from the links found in each RSS Feed when they don't have a 'title' or 'content' key available.
    
    - Created RssPull class which uses an initialized URLParser and a list of RSS Feeds and creates an output dictionary containing the published date, author(s), url, title, and content for all entries in each RSS Feed
    
    - Created DataTransformer class to handle data preprocessing. Currently handles datetime cleanup, title cleanup, and content cleanup.

--- rss_pull_upgraded.py
+++ rss_pull_upgraded.py
@@ -24,112 +24,184 @@ def timer(label):
     logger.info(f"{label}: {time} seconds")
 
 
-def parse_feed(feed_url):
-    return feedparser.parse(feed_url)
+class DataTransformer:
+    def __init__(self):
+        self.date_format = "%a, %d %b %Y %H:%M:%S %z"
+        self.reddit_content_prefixes = [
+            "Chapel Hill, NC",
+            "A subreddit for the city (and county) of Durham, North Carolina.",
+            'Raleigh is the capital of the state of North Carolina as well as the seat of Wake County. Raleigh is known as the "City of Oaks" for its many oak trees. Join us on Discord! https://discord.gg/PPCARNjJAg',
+        ]
+
+    def clean_published_dates(self, dataframe):
+        date_list = dataframe["published"]
+        # Parse each string into a datetime object
+        parsed_dates = [
+            datetime.strptime(date_string, self.date_format)
+            for date_string in date_list
+        ]
+        # Convert datetime objects to strings in the desired format
+        dataframe["cleaned_published"] = [
+            date.strftime("%Y-%m-%d %H:%M:%S") for date in parsed_dates
+        ]
+
+        return dataframe
+
+    def clean_titles(self, dataframe):
+        title_list = dataframe["title"]
+
+        replace_newlines = self.replace_newlines_and_slashes(title_list)
+
+        # Processing the list to remove " - " at the end of each string
+        replace_suffixes = [title.rstrip(" - ") for title in replace_newlines]
+
+        # Remove leading and trailing whitespace from each element
+        dataframe["cleaned_title"] = [s.strip() for s in replace_suffixes]
+
+        return dataframe
+
+    def replace_newlines_and_slashes(self, data_list):
+        # Replace any instances of \n or \' in the title with nothing or ', respectively
+        return [info.replace("\n", "").replace("\\'", "'") for info in data_list]
+
+    def clean_content(self, dataframe):
+        content_list = dataframe["content"]
+
+        replace_newlines = self.replace_newlines_and_slashes(content_list)
+
+        # Iterate through each element and check for prefixes
+        for i, element in enumerate(replace_newlines):
+            for prefix in self.reddit_content_prefixes:
+                if element.startswith(prefix):
+                    # Replace the prefix with whitespace
+                    replace_newlines[i] = element.replace(
+                        prefix, "", 1
+                    )  # Replace only the first occurrence
+                    break  # Stop checking other prefixes if one has already matched
+
+        dataframe["cleaned_content"] = [s.strip() for s in replace_newlines]
+
+        return dataframe
 
 
-def parse_page(page):
-    return BeautifulSoup(page, "html.parser")
+class URLParser:
+    def __init__(self) -> None:
+        pass
+
+    def parse_page(self, page):
+        return BeautifulSoup(page, "html.parser")
+
+    def is_valid_url(self, url):
+        try:
+            result = urlparse(url)
+            return all([result.scheme, result.netloc])
+        except Exception:
+            return False
+
+    def extract_url_content(self, url):
+        if self.is_valid_url(url):
+            page = requests.get(url).text
+            soup = self.parse_page(page)
+            item = soup.find_all("p")
+            return " ".join([" ".join(text.get_text().split()) for text in item])
+        else:
+            logger.warning(f"Invalid URL - Content: {url}")
+            return None
+
+    def extract_url_title(self, url):
+        if self.is_valid_url(url):
+            page = requests.get(url).text
+            soup = self.parse_page(page)
+            description = soup.find_all("h1")
+            header = "".join(f"{content.get_text()}" for content in description)
+            title = f"{header}" if "reddit" in url else f"{soup.title.get_text()}"
+            return title
+        else:
+            logger.warning(f"Invalid URL - Titles: {url}")
+            return None
 
 
-def is_valid_url(url):
-    try:
-        result = urlparse(url)
-        return all([result.scheme, result.netloc])
-    except Exception:
-        return False
+class RssPull:
+    def __init__(self, url_parser, feed_list):
+        self.url_parser = url_parser
+        self.feed_list = feed_list
 
+    def parse_feed(self, url):
+        return feedparser.parse(url)
 
-def extract_content(url):
-    if is_valid_url(url):
-        page = requests.get(url).text
-        soup = parse_page(page)
-        item = soup.find_all("p")
-        return " ".join([" ".join(text.get_text().split()) for text in item])
-    else:
-        logger.warning(f"Invalid URL - Content: {url}")
-        return None
+    def pull_feed(self):
+        all_urls = []
+        all_published_dates = []
+        all_authors = []
+        all_titles = []
+        all_content = []
 
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.parse_feed, url) for url in self.feed_list]
 
-def extract_titles(url):
-    if is_valid_url(url):
-        page = requests.get(url).text
-        soup = parse_page(page)
-        description = soup.find_all("h1")
-        header = "".join(f"{content.get_text()}" for content in description)
-        title = f"{header}" if "reddit" in url else f"{soup.title.get_text()}"
-        return title
-    else:
-        logger.warning(f"Invalid URL - Titles: {url}")
-        return None
+            for future in concurrent.futures.as_completed(futures):
+                feed = future.result()
 
+                for item in feed.entries:
+                    all_published_dates.append(self.extract_published_date(item))
+                    all_authors.append(self.extract_authors(item))
+                    all_urls.append(self.extract_urls(item))
+                    all_titles.append(self.extract_titles(item))
+                    all_content.append(self.extract_content(item))
 
-def build_dataframe(published, authors, all_urls, title, article_content):
-    df_dict = {
-        "published": published,
-        "authors": authors,
-        "urls": all_urls,
-        "title": title,
-        "article_content": article_content,
-    }
+        return {
+            "published": all_published_dates,
+            "authors": all_authors,
+            "urls": all_urls,
+            "title": all_titles,
+            "content": all_content,
+        }
 
-    return pd.DataFrame(
-        df_dict, columns=["published", "authors", "urls", "title", "article_content"]
-    )
+    def extract_published_date(self, item):
+        with timer("Extracting publish date"):
+            return item.published if hasattr(item, "published") else "Unknown date"
 
+    def extract_authors(self, item):
+        with timer("Extracting authors"):
+            return item.author if hasattr(item, "author") else "Unknown author"
 
-def clean_published_dates(date_list):
-    # Define the date format
-    date_format = "%a, %d %b %Y %H:%M:%S %z"
+    def extract_urls(self, item):
+        with timer("Extracting URLs"):
+            return item.link if hasattr(item, "link") else "Unknown link"
 
-    # Parse each string into a datetime object
-    parsed_dates = [
-        datetime.strptime(date_string, date_format) for date_string in date_list
-    ]
+    def extract_titles(self, item):
+        with timer("Extracting titles"):
+            if hasattr(item, "title"):
+                return item.title
+            title_results = self.url_parser.extract_url_title(item.link)
+            return title_results if title_results is not None else "Unknown title"
 
-    # Convert datetime objects to strings in the desired format
-    return [date.strftime("%Y-%m-%d %H:%M:%S") for date in parsed_dates]
+    def extract_content(self, item):
+        with timer("Extracting content"):
+            if hasattr(item, "content") and "reddit" in item.link:
+                # Extracting the HTML part from your list element
+                html_content = item.content[0]["value"]
 
+                # Parsing the HTML with BeautifulSoup
+                soup = self.url_parser.parse_page(html_content)
 
-def clean_titles(title_list):
-    replace_newlines = replace_newlines_and_slashes(title_list)
+                # Extracting text from all <p> tags if there are any <p> tags
+                if len(soup.find_all("p")) > 0:
+                    for p in soup.find_all("p"):
+                        content = p.get_text()
+                else:
+                    content = ""
 
-    # Processing the list to remove " - " at the end of each string
-    replace_suffixes = [title.rstrip(" - ") for title in replace_newlines]
-
-    # Remove leading and trailing whitespace from each element
-    return [s.strip() for s in replace_suffixes]
-
-
-def replace_newlines_and_slashes(data_list):
-    # Replace any instances of \n or \' in the title with nothing or ', respectively
-    return [
-        info.replace("\n", "").replace("\\'", "'").replace("\\\\'", "'")
-        for info in data_list
-    ]
-
-
-def clean_content(content_list):
-    replace_newlines = replace_newlines_and_slashes(content_list)
-
-    # List of prefixes
-    prefixes = [
-        "Chapel Hill, NC",
-        "A subreddit for the city (and county) of Durham, North Carolina.",
-        'Raleigh is the capital of the state of North Carolina as well as the seat of Wake County. Raleigh is known as the "City of Oaks" for its many oak trees. Join us on Discord! https://discord.gg/PPCARNjJAg',
-    ]
-
-    # Iterate through each element and check for prefixes
-    for i, element in enumerate(replace_newlines):
-        for prefix in prefixes:
-            if element.startswith(prefix):
-                # Replace the prefix with whitespace
-                replace_newlines[i] = element.replace(
-                    prefix, "", 1
-                )  # Replace only the first occurrence
-                break  # Stop checking other prefixes if one has already matched
-
-    return [s.strip() for s in replace_newlines]
+                return content
+            elif (
+                hasattr(item, "content")
+                and "abc11" in item.link
+                or not hasattr(item, "content")
+            ):
+                content_results = self.url_parser.extract_url_content(item.link)
+                return content_results if content_results is not None else ""
+            else:
+                return item.content
 
 
 feed_list = [
@@ -142,74 +214,22 @@ feed_list = [
     "https://reddit.com/r/bullcity/new/.rss?sort=new",
 ]
 
-all_urls = []
-published = []
-authors = []
-titles = []
-article_content = []
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    futures = [executor.submit(parse_feed, url) for url in feed_list]
+parser = URLParser()
 
-    for future in concurrent.futures.as_completed(futures):
-        feed = future.result()
+test = RssPull(parser, feed_list)
+test_dict = test.pull_feed()
 
-        for item in feed.entries:
-            with timer("Extracting publish date"):
-                if hasattr(item, "published"):
-                    published.append(item.published)
-                else:
-                    published.append("Unknown date")
+dict_cols = list(test_dict.keys())
+initial_df = pd.DataFrame(test_dict, columns=dict_cols)
 
-            with timer("Extracting authors"):
-                try:
-                    authors.append(item.author)
-                except AttributeError:
-                    authors.append("Unknown")
+preprocessor = DataTransformer()
 
-            with timer("Extracting URLs"):
-                if hasattr(item, "link"):
-                    all_urls.append(item.link)
-                else:
-                    all_urls.append("Unknown link")
+test_df = preprocessor.clean_published_dates(initial_df)
+print(test_df.info())
 
-            with timer("Extracting titles"):
-                if hasattr(item, "title"):
-                    titles.append(item.title)
-                else:
-                    title_results = extract_titles(item.link)
-                    if title_results is not None:
-                        titles.append(title_results)
+test_df2 = preprocessor.clean_titles(test_df)
+print(test_df2.info())
 
-            with timer("Extracting content"):
-                if hasattr(item, "content"):
-                    print(item.link)
-                    print(item.content)
-                    if "reddit" in item.link:
-                        # Extracting the HTML part from your list element
-                        html_content = item.content[0]["value"]
-
-                        # Parsing the HTML with BeautifulSoup
-                        soup = parse_page(html_content)
-
-                        # Extracting text from all <p> tags
-                        article_content.append(p.get_text() for p in soup.find_all("p"))
-                    elif "abc11" in item.link:
-                        content_results = extract_content(item.link)
-                        if content_results is not None:
-                            article_content.append(content_results)
-                    else:
-                        article_content.append(item.content)
-                else:
-                    content_results = extract_content(item.link)
-                    if content_results is not None:
-                        article_content.append(content_results)
-
-
-initial_daily_df = build_dataframe(
-    published, authors, all_urls, titles, article_content
-)
-
-print(initial_daily_df.head())
-print(initial_daily_df.tail())
-print(initial_daily_df.info())
+test_df3 = preprocessor.clean_content(test_df2)
+print(test_df3.info())
